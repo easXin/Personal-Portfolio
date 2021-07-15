3b7f33132d799fb9db0ac47cacbb959fd5279985
easxin <codingericwang@gmail.com>
2021-07-15 11:05:02 -0400

    finish profile section

--- src/components/body/about/Profile.js
+++ src/components/body/about/Profile.js
@@ -11,14 +11,18 @@ function Profile() {
                     <span>
                         <Typewriter
                             options={{
-                                strings: [' Full Stack Developer', ' Problem Solver', ' Hiker'],
+                                strings: ['Full Stack Developer', 'Problem Solver', 'Hiker'],
                                 autoStart: true,
                                 loop: true,
-                                pauseFor: 2500
+                                pauseFor: 2500,
+                                deleteSpeed: 30
                             }}
                         />
                     </span>
                 </p>
+                <h3>
+                    Based in <span>New York.</span> I like hiking, coding and using React
+                </h3>
             </div>
         </section>
     )
