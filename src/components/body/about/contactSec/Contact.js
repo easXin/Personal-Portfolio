c4e67860fa2a128fa30c50841446d8f30ae510b9
easxin <codingericwang@gmail.com>
2021-07-16 03:55:56 -0400

    finish the layout of contact section

--- src/components/body/about/contactSec/Contact.js
+++ src/components/body/about/contactSec/Contact.js
@@ -1,5 +1,9 @@
 import React from 'react'
 import './Contact.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Form from 'react-bootstrap/Form'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
 function Contact() {
     return (
         <div className="contact">
@@ -14,7 +18,16 @@ function Contact() {
                 </p>
             </div>
             <div className="contact__detail">
-
+                <Form>
+                    <Row>
+                        <Col>
+                            <Form.Control placeholder="First name" />
+                        </Col>
+                        <Col>
+                            <Form.Control placeholder="Last name" />
+                        </Col>
+                    </Row>
+                </Form>
             </div>
         </div>
     )
