21dbd1549a8051c21219cf5bc20abd3d8f866054
easxin <codingericwang@gmail.com>
2021-07-17 13:15:11 -0400

    connect frontend to local storage json

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,10 +1,10 @@
-import React from 'react'
+import React, { useState, useMemo } from 'react'
 import { Avatar, IconButton } from '@material-ui/core';
 import HeaderOptions from './HeaderOptions';
 import './Header.css'
 import { Icon } from '@iconify/react';
 import bxlFacebook from '@iconify-icons/bx/bxl-facebook';
-import bxlGooglePlus from '@iconify-icons/bx/bxl-google-plus';
+import bxlSlack from '@iconify-icons/bx/bxl-slack';
 import bxlInstagram from '@iconify-icons/bx/bxl-instagram';
 import bxlLinkedin from '@iconify-icons/bx/bxl-linkedin';
 import bxGitBranch from '@iconify-icons/bx/bx-git-branch';
@@ -15,20 +15,31 @@ import AssignmentIcon from '@material-ui/icons/Assignment';
 import ChromeReaderModeIcon from '@material-ui/icons/ChromeReaderMode';
 import EmailIcon from '@material-ui/icons/Email';
 
-function Header() {
+function Header({ data }) {
+    let name = null;
+    let socialLink = null;
+    let imgUrl = "./images/";
+    if (data) {
+        name = data.name;
+        socialLink = data.social;
+        imgUrl = data.image;
+    }
+
+    onclick()
+
     return (
         <div className="header">
             <div className="header__profile">
-                <Avatar src="logo192.png" />
+                <Avatar src="./images/profile.jpg" />
                 <h1>
-                    <a href="javascript();">Eric Wang</a>
+                    <a href="javascript();">{name}</a>
                 </h1>
                 <div className="header__icons">
                     <IconButton>
                         <Icon icon={bxlFacebook} />
                     </IconButton>
                     <IconButton>
-                        <Icon icon={bxlGooglePlus} />
+                        <Icon icon={bxlSlack} />
                     </IconButton>
                     <IconButton>
                         <Icon icon={bxlInstagram} />
@@ -57,11 +68,6 @@ function Header() {
                 <a href="#contact">
                     <HeaderOptions Icon={EmailIcon} title="Contact" />
                 </a>
-
-
-
-
-
             </div>
             <div className="header__footer">
                 <div className="header__copyrights">
