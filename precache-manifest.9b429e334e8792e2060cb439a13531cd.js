05d234897bf400f3713b0554e6c57e34ea7b161b
easxin <codingericwang@gmail.com>
2023-09-15 17:15:21 -0500

    Updates

---
+++ precache-manifest.9b429e334e8792e2060cb439a13531cd.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "a2f7bf70b7e2363c4046",
+    "url": "/portfolio/static/css/main.3d81f0eb.chunk.css"
+  },
+  {
+    "revision": "a2f7bf70b7e2363c4046",
+    "url": "/portfolio/static/js/main.a2f7bf70.chunk.js"
+  },
+  {
+    "revision": "61699e6bdd712f4ffbd0",
+    "url": "/portfolio/static/css/1.64bbe021.chunk.css"
+  },
+  {
+    "revision": "61699e6bdd712f4ffbd0",
+    "url": "/portfolio/static/js/1.61699e6b.chunk.js"
+  },
+  {
+    "revision": "ee2a100325fd284f0097",
+    "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
+  },
+  {
+    "revision": "8f5de81cb71aac61e8aac0b02cd3f08a",
+    "url": "/portfolio/static/media/dw.8f5de81c.jpg"
+  },
+  {
+    "revision": "19d9f5df24fc6ab27a479852fc73ff5a",
+    "url": "/portfolio/static/media/pic.19d9f5df.jpg"
+  },
+  {
+    "revision": "62238792a26f1957ee104fd17a54aaf8",
+    "url": "/portfolio/static/media/dog-confused.62238792.gif"
+  },
+  {
+    "revision": "0504f7c370e2458976c39da2bb445497",
+    "url": "/portfolio/index.html"
+  }
+];
